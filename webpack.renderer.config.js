8f8c200eb9d258abc051e3adf9bea188c8a83d46
suyoin <19378617+suyoin@users.noreply.github.com>
2021-03-05 14:30:55 -0600

    static image serve

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,17 +1,26 @@
-const rules = require('./webpack.rules');
-const plugins = require('./webpack.plugins');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const rules = require("./webpack.rules");
+const plugins = require("./webpack.plugins");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+	test: /\.css$/,
+	use: [{ loader: "style-loader" }, { loader: "css-loader" }],
 });
 
+plugins.push(
+	new copyWebpackPlugin({
+		patterns: [{ from: path.resolve(__dirname, "static"), to: "static" }],
+	})
+);
+
 module.exports = {
-  module: {
-    rules,
-  },
-  plugins: plugins,
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
-  },
+	module: {
+		rules,
+	},
+	plugins: plugins,
+	resolve: {
+		extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
+	},
 };
