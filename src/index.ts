5cdd41703ce4feed3a14e0f5c52cb2eda4f9072c
suyoin <19378617+suyoin@users.noreply.github.com>
2021-04-25 08:41:49 -0500

    tag v

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { getCurrentlyPlaying, oauth2Callback } from "./util/spotifyOAuth2";
 import { join } from "path";
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
 	// eslint-disable-line global-require
 	app.quit();
